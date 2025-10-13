5ab34c4b09fbb6447a97060f6a08086a31ac8e7e
shlneo <TW1@kvantas.ent>
2025-10-13 11:18:41 +0300

    fetch: add proj

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # porsche
+
+![Python](https://img.shields.io/badge/python-3.12-blue)
+[![Built by Shlneo](https://img.shields.io/badge/Built%20by-Shlneo%20-blue)](https://github.com/shlneo)
+
+## Info
+ 
+### Trying to make a porsche website.
+
+## Requirements
+
+- python `3.12.0`
+- add `.env`
+- PostgreSql `17`
+
+##Database Settings
+
+1. Make a user `...` and give him pass `...`.
+
+2. Create database `porsche_db` with superuser `...`.
+
+## Version
+ 
+1.0.0
+
+## Launch
+
+```bash 
+python main.py
+```
---
+++ app/__init__.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, session, request, g, redirect, url_for
+from flask_babel import Babel
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_babel import Babel, format_date
+from .completion_db import create_database
+from flask_wtf.csrf import CSRFProtect
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+socketio = SocketIO()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+migrate = Migrate()
+
+csrf = CSRFProtect()
+
+LANGUAGES = {
+    'en': 'English',
+    'ru': 'Русский',
+    'be': 'Беларуский'
+}
+
+def get_locale():
+    if 'language' in session and session['language'] in LANGUAGES:
+        return session['language']
+
+    user = getattr(g, 'user', None)
+    if user and hasattr(user, 'locale') and user.locale in LANGUAGES:
+        return user.locale
+
+    return request.accept_languages.best_match(LANGUAGES)
+
+def get_timezone():
+    user = getattr(g, 'user', None)
+    if user is not None and hasattr(user, 'timezone'):
+        return user.timezone
+    return None
+
+babel = Babel(
+    locale_selector=get_locale,
+    timezone_selector=get_timezone
+)
+
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+
+    app.config.update(
+        SECRET_KEY=os.getenv('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI=f"postgresql://{os.getenv('postrgeuser')}:{os.getenv('postrgepass')}@localhost:5432/{os.getenv('postrgedbname')}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        BABEL_DEFAULT_LOCALE='ru',
+        BABEL_TRANSLATION_DIRECTORIES = 'translations',
+        LANGUAGES=LANGUAGES,
+        SESSION_SQLALCHEMY = db,
+        SESSION_PERMANENT = True,
+        SESSION_TYPE = 'sqlalchemy'
+    )
+
+    db.init_app(app)
+    socketio.init_app(app)
+    babel.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+    csrf.init_app(app)
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    with app.app_context():
+        db.create_all()
+        create_database(app, db)
+
+    from .models import User, Car
+    
+    login_manager.init_app(app)
+    login_manager.login_message = "Need to auth."
+    login_manager.login_view = "auth.login"
+    
+    app.jinja_env.globals['format_date'] = format_date
+    
+    @app.context_processor
+    def inject_get_locale():
+        from . import get_locale
+        return dict(get_locale=get_locale)
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html', hide_header = True), 404
+    
+
+    return app
